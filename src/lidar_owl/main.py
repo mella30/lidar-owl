3feb50b3c3072bdf170b4a215c84550d2c71093e
Mariella Dreissig <melladreissig@gmail.com>
2025-10-19 12:15:53 +0200

    env fixes; dataloading tested

--- src/lidar_owl/main.py
+++ src/lidar_owl/main.py
@@ -1,14 +1,24 @@
-import open3d as o3d
-import numpy as np
+import os
+import open3d.ml.torch as ml3d
+
+DATA_DIR = '/data/'
 
 def main():
-    print("Welcome to the Lidar Owl application!")
-    # Test Open3D import by creating and printing a PointCloud object
-    # Erstelle eine zufällige Punktwolke mit 100 Punkten
-    points = np.random.rand(100, 3)
-    pcd = o3d.geometry.PointCloud()
-    pcd.points = o3d.utility.Vector3dVector(points)
-    print("Open3D PointCloud object created:", np.asarray(pcd.points))
+    # construct a dataset by specifying dataset_path
+    dataset = ml3d.datasets.SemanticKITTI(dataset_path=os.path.join(DATA_DIR, 'datasets', 'public_datasets', 'semantic_kitti'))
+
+    # get the 'all' split that combines training, validation and test set
+    all_split = dataset.get_split('all')
+
+    # print the attributes of the first datum
+    print(all_split.get_attr(0))
+
+    # print the shape of the first point cloud
+    print(all_split.get_data(0)['point'].shape)
+
+    # show the first 100 frames using the visualizer
+    vis = ml3d.vis.Visualizer()
+    vis.visualize_dataset(dataset, 'all', indices=range(100))
 
 if __name__ == "__main__":
     main()
