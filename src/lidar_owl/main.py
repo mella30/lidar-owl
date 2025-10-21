dc46715c808b078f9c66d779da0a1dfa88f40704
Mariella Dreissig <melladreissig@gmail.com>
2025-10-21 11:12:28 +0200

    model loading working

--- src/lidar_owl/main.py
+++ src/lidar_owl/main.py
@@ -1,24 +1,26 @@
 import os
-import open3d.ml.torch as ml3d
+import open3d.ml.torch as ml3d_torch
+import open3d.ml as ml3d
 
 DATA_DIR = '/data/'
 
 def main():
+    # read config file
+    # config = yaml.safe_load(open(os.path.join('configs', 'pointpillars_kitti.yaml')))
+    cfg_file = os.path.join('configs', 'randlanet_semantickitti.yaml')
+    cfg = ml3d.utils.Config.load_from_file(cfg_file)
+    
     # construct a dataset by specifying dataset_path
-    dataset = ml3d.datasets.SemanticKITTI(dataset_path=os.path.join(DATA_DIR, 'datasets', 'public_datasets', 'semantic_kitti'))
+    cfg.dataset['dataset_path'] = os.path.join(DATA_DIR, 'datasets', 'public_datasets', 'semantic_kitti')
+    dataset = ml3d_torch.datasets.SemanticKITTI(**cfg.dataset)
 
-    # get the 'all' split that combines training, validation and test set
-    all_split = dataset.get_split('all')
+    model = ml3d_torch.models.RandLANet(**cfg.model)
 
-    # print the attributes of the first datum
-    print(all_split.get_attr(0))
+    pipeline = ml3d_torch.pipelines.SemanticSegmentation(model=model, dataset=dataset, max_epoch=100)
+    pipeline.run_train()
 
-    # print the shape of the first point cloud
-    print(all_split.get_data(0)['point'].shape)
+    print("Training and testing completed.")
 
-    # show the first 100 frames using the visualizer
-    vis = ml3d.vis.Visualizer()
-    vis.visualize_dataset(dataset, 'all', indices=range(100))
 
 if __name__ == "__main__":
     main()
---
+++ src/lidar_owl/train.py
@@ -0,0 +1,5 @@
+import open3d.ml.torch as ml3d
+
+
+def train():
+    pass  # Training logic goes here
