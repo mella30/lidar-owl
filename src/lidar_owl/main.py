162c26da09fe8b0225e85bc68d7672075439b1c4
Mariella Dreissig <melladreissig@gmail.com>
2025-11-19 11:56:43 +0100

    checkpoint saving, pipeline (run/test) refactoring, several confdict fixes

--- src/lidar_owl/main.py
+++ src/lidar_owl/main.py
@@ -1,27 +1,23 @@
 import hydra
 import open3d as o3d
-from omegaconf import DictConfig
+import omegaconf as OC
 
 from train import Trainer
 from eval import Evaluator
 
 
 @hydra.main(version_base=None, config_path="../../configs", config_name="config")
-def main(cfg: DictConfig):
-    # keep console spam low
-    o3d.utility.set_verbosity_level(o3d.utility.VerbosityLevel.Info)  # Error
-
+def main(cfg: OC.DictConfig):
     # compact debug setup
     if cfg.get("debug"):
         cfg.dataset.training_split = ['08']
         cfg.dataset.validation_split = ['08']
         cfg.pipeline.max_epoch = 25
+        cfg.pipeline.save_ckpt_freq = 0
 
-    # TODO: checkpoint should never be loaded for train! for eval, open3d default (last one) is fine
-    if cfg.mode in ("train_eval", "train+eval", "both"):
+    if cfg.mode == "train+eval":
         semseg_trainer = Trainer(cfg)
         semseg_trainer.train()
-        # reuse trained pipeline to directly run evaluation with trained weights
         semseg_trainer.pipeline.run_test()
     elif cfg.mode == "train":
         semseg_trainer = Trainer(cfg)
--- src/lidar_owl/train.py
+++ src/lidar_owl/train.py
@@ -1,7 +1,7 @@
 from typing import Any
 
 import open3d.ml.torch as ml3d
-from omegaconf import OmegaConf
+from omegaconf import OmegaConf as OC
 
 from ml3d_util import get_dataset, get_model, get_pipeline
 
@@ -10,19 +10,9 @@ class Trainer:
     def __init__(self, config: dict[str, Any]):
         self.config = config  # incl model, dataset & pipeline
 
-        # build model without loading a checkpoint unless a path is provided
-        model_cfg = OmegaConf.to_container(config.model, resolve=True, enum_to_str=True)
-        model_name = model_cfg.get("name")
-        model_cls = get_model(model_name)
-        ckpt_path = model_cfg.pop("ckpt_path", None)
-        # remove meta keys not part of ctor signature
-        model_cfg.pop("name", None)
-        if ckpt_path:
-            self.model = model_cls(ckpt_path=ckpt_path, **model_cfg)
-        else:
-            self.model = model_cls(**model_cfg)
-        self.dataset = get_dataset(self.config["dataset"]["name"])(**config.dataset)
-        self.pipeline = get_pipeline(self.config["pipeline"]["name"])(self.model, self.dataset, **config.pipeline)
+        self.model = get_model(self.config["model"]["name"])(**OC.to_container(config.model))
+        self.dataset = get_dataset(self.config["dataset"]["name"])(**OC.to_container(config.dataset))
+        self.pipeline = get_pipeline(self.config["pipeline"]["name"])(self.model, self.dataset, **OC.to_container(config.pipeline))
 
     def train(self):
         self.pipeline.run_train()
